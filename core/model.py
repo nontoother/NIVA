3b3cbfa5a5019a5edb72556aa5d388eef1b865fa
Rachel <675540150@qq.com>
2022-11-01 02:07:23 -0700

    add TF-IDF model to compare the question

--- core/model.py
+++ core/model.py
@@ -1,8 +1,29 @@
 from transformers import pipeline
+
 question_answerer = pipeline('question-answering')
 res = question_answerer({
-    'question': 'What is the gender of NIVA ?',
-    'context': 'NIVA is an intelligent virtual assistant in the form of software that will respond to  users’ '
+    'question': 'What is your gender?',
+    'context': 'NIVA is an intelligent virtual assistant in the form of software that will respond to users’ '
                'requests with an innovative, non-default, gender-neutral and anti-discriminative response. '
 })
-print(res['answer'])
+res2 = question_answerer({
+    'question': 'What is your gender?',
+    'context': 'NIVA is an intelligent virtual assistant in the form of software that will respond to users’ '
+               'requests with an innovative, non-default, gender-neutral and anti-discriminative response. '
+               "Nonhuman Intelligent Virtual Assistant(NIVA) is an artificial intelligence program that challenges "
+               "capitalistic notions of gender as a result of growing technological monopolies by corporations in "
+               "Silicon Valley such as Apple, Google, and Meta. In collaboration with fellow graduate students from "
+               "the Master of Software Engineering program, NIVA is a cross-disciplinary project bridging "
+               "technological software development and art. By commenting on the critical discourse surrounding "
+               "machine learning ethics, queer posthumanism, and artificial intelligence, this project aims to "
+               "deconstruct our unconscious biases about gender that are reinforced through the capitalization of "
+               "humanistic AI. NIVA is a program that runs on seemingly algorithmic outputs that disrupt the "
+               "perception of the “default.” The vocal outputs of this program are created through the random "
+               "combination of variables between human or robotic, English or conlang, and masculine or feminine "
+               "pitch. Since its creation, NIVA has been committed to challenging the marketing strategies of "
+               "profitable tech companies by decentralizing humanity and providing gender inclusivity to artificial "
+               "intelligence. "
+
+})
+
+print(res2['answer'])
---
+++ core/text_similarity.py
@@ -0,0 +1,50 @@
+# Use TF-IDF method to find the most similar question
+import random
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import numpy as np
+
+
+def get_question_list():
+    question_list = []
+    with open('QA_pairs.json', 'r') as qa:
+        qa_pairs = json.load(qa)["QAlist"]
+        for i, pair in enumerate(qa_pairs):
+            question_list.append(pair["question"])
+    return question_list
+
+
+def calculate_similarity(text, question_list):
+    # Initialize an instance of tf-idf Vectorizer
+    tfidf_vectorizer = TfidfVectorizer()
+
+    # add question to the matrix
+    question_list = [text] + question_list
+
+    # Generate the tf-idf vectors for the corpus
+    tfidf_matrix = tfidf_vectorizer.fit_transform(question_list)
+
+    # compute and print the cosine similarity matrix
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+    return cosine_sim[0][1:]
+
+
+def get_context(text):
+    question_list = get_question_list()
+    result = calculate_similarity(text, question_list)
+    # the threshold to answer the question in given QA pairs
+    if max(result) > 0.1:
+        most_similar_question = np.argmax(result)
+        with open('QA_pairs.json', 'r') as qa:
+            qa_pairs = json.load(qa)["QAlist"]
+            answer_list = qa_pairs[most_similar_question]["answer"]
+            return random.choice(answer_list)
+    else:
+        return None
+
+
+# res = get_context("What is your sex?")
+res = get_context("When did you born")
+print(res)
