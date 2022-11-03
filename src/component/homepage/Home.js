f68dc41b2e6fc554f8096a185640d6fb01bd7b33
andyJiayiLi <lijiayi606@gmail.com>
2022-11-03 16:15:56 -0700

    applied eslint

--- src/component/homepage/Home.js
+++ src/component/homepage/Home.js
@@ -1,77 +1,73 @@
-import { Component, useState } from 'react'
-import axios from "axios";
+import React, { useState } from 'react'
+// import axios from 'axios'
 import AudioAnalyser from '../voice/AudioAnalyser'
-import logo from '../../resources/logo2.svg';
-import Waiting from '../text/Waiting';
-import Notification from '../modal/Notification';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import logo from '../../resources/logo2.svg'
+import Waiting from '../text/Waiting'
+import Notification from '../modal/Notification'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-import './Home.css';
+import './Home.css'
 
-export default function Home() {
+export default function Home () {
 
-    const [audio, setAudio] = useState(null)
-    const [access, setAccess] = useState(false)
-    const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition,
-        isMicrophoneAvailable
-      } = useSpeechRecognition();
+  const [audio, setAudio] = useState(null)
+  const [access, setAccess] = useState(false)
+  const {
+    transcript,
+    resetTranscript
+  } = useSpeechRecognition()
 
-    function checkPermissions() {
-        const permissions = navigator.mediaDevices.getUserMedia({audio: true, video: false})
-        permissions.then((stream) => {
-          setAccess(!access)
-        })
-        .catch((err) => {
-          setAccess(false)
-          console.log(`${err.name} : ${err.message}`)
-        });
-      }
-    
-    async function getMicrophone() {
-        const audio = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: false
-        });
-        SpeechRecognition.startListening({ continuous: true })
-        setAudio(audio)
+  function checkPermissions () {
+    const permissions = navigator.mediaDevices.getUserMedia({audio: true, video: false})
+    permissions.then((stream) => {
+      setAccess(!access)
+    })
+      .catch((err) => {
+        setAccess(false)
+        console.log(`${err.name} : ${err.message}`)
+      })
+  }
+
+  async function getMicrophone () {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    })
+    SpeechRecognition.startListening({ continuous: false })
+    setAudio(audio)
+  }
+
+  function stopMicrophone () {
+    SpeechRecognition.stopListening()
+    audio.getTracks().forEach(track => track.stop())
+    setAudio(null)
+  }
+
+  function toggleMicrophone () {
+    resetTranscript()
+    if (audio) {
+      stopMicrophone()
+    } else {
+      getMicrophone()
     }
+  }
 
-    function stopMicrophone() {
-        SpeechRecognition.stopListening()
-        audio.getTracks().forEach(track => track.stop());
-        setAudio(null)
-        
-    }
+  checkPermissions()
 
-    function toggleMicrophone() {
-        resetTranscript()
-        if (audio) {
-          stopMicrophone();
-        } else {
-          getMicrophone();
-        }
-      }
-
-    checkPermissions()
-
-    return (
-        <div className="App">
-            <Notification status={access} askAccess={checkPermissions}/>
-            <img src={logo} className="App-logo" alt="logo" />
-            {!audio && <Waiting/>}
-            {audio ? <AudioAnalyser audio={audio} /> : ''}
-            <p id='transcript'>{transcript}</p>          
-            <div className="controls">
-                <button onClick={toggleMicrophone}>
-                {audio ? 'Stop' : 'Start Talking'}
-                </button>
-            </div>
-        </div>
-        );
+  return (
+    <div className="App">
+      <Notification status={access} askAccess={checkPermissions}/>
+      <img src={logo} className="App-logo" alt="logo" />
+      {!audio && <Waiting/>}
+      {audio ? <AudioAnalyser audio={audio} /> : ''}
+      <p id='transcript'>{transcript}</p>
+      <div className="controls">
+        <button onClick={toggleMicrophone}>
+          {audio ? 'Stop' : 'Start Talking'}
+        </button>
+      </div>
+    </div>
+  )
 
 }
 
--- src/component/modal/Notification.js
+++ src/component/modal/Notification.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import ReactModal from 'react-modal';
+import React, { useState } from 'react'
+import ReactModal from 'react-modal'
 import './Notification.css'
 
-export default function Notification(props) {
-    const [showModal, setShowModal] = useState({...props.status})
+export default function Notification (props) {
+  const [showModal, setShowModal] = useState({...props.status})
 
-    function closeModal() {
-        setShowModal(false)
-        props.askAccess()
-    }
-    
-    return (
-        <div>
-            <ReactModal 
-            isOpen={showModal}
-            className="Modal"
-            overlayClassName="Overlay"
-            >
-            <p>Please allow the microphone access</p>
-            <button onClick={closeModal}>Okay</button>
-            </ReactModal>
-        </div>
-    );
+  function closeModal () {
+    setShowModal(false)
+    props.askAccess()
+  }
+
+  return (
+    <div>
+      <ReactModal
+        isOpen={showModal}
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <p>Please allow the microphone access</p>
+        <button onClick={closeModal}>Okay</button>
+      </ReactModal>
+    </div>
+  )
 }
 
