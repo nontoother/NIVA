39a29bb2d45b3cd7d78eaba7c27f8718b3e07305
matiarasianipar <77903930+matiarasianipar@users.noreply.github.com>
2022-11-10 18:07:24 -0800

    Frontend Updates
    - Logo matches Figma prototype
    - Click to activate, Listening state, Loading state
    - Commented out audio wave visualizer

--- src/component/voice/AudioVisualiser.js
+++ src/component/voice/AudioVisualiser.js
@@ -5,34 +5,36 @@ class AudioVisualiser extends Component {
     super(props)
     this.canvas = React.createRef()
   }
-  draw () {
-    const { audioData } = this.props
-    const canvas = this.canvas.current
-    const height = canvas.height
-    const width = canvas.width
-    const context = canvas.getContext('2d')
-    let x = 0
-    const sliceWidth = (width * 1.0) / audioData.length
-    context.lineWidth = 2
-    context.strokeStyle = '#000000'
-    context.clearRect(0, 0, width, height)
-    context.beginPath()
-    context.moveTo(0, height / 2)
-    for (const item of audioData) {
-      const y = (item / 255.0) * height
-      context.lineTo(x, y)
-      x += sliceWidth
-    }
-    context.lineTo(x, height / 2)
-    context.stroke()
-  }
 
-  componentDidUpdate () {
-    this.draw()
-  }
+  // AUDIO WAVELENGTHS
+  // draw () {
+  //   const { audioData } = this.props
+  //   const canvas = this.canvas.current
+  //   const height = canvas.height
+  //   const width = canvas.width
+  //   const context = canvas.getContext('2d')
+  //   let x = 0
+  //   const sliceWidth = (width * 1.0) / audioData.length
+  //   context.lineWidth = 2
+  //   context.strokeStyle = '#000000'
+  //   context.clearRect(0, 0, width, height)
+  //   context.beginPath()
+  //   context.moveTo(0, height / 2)
+  //   for (const item of audioData) {
+  //     const y = (item / 255.0) * height
+  //     context.lineTo(x, y)
+  //     x += sliceWidth
+  //   }
+  //   context.lineTo(x, height / 2)
+  //   context.stroke()
+  // }
+
+  // componentDidUpdate () {
+  //   this.draw()
+  // }
 
   render () {
-    return <canvas width="200" height="200" ref={this.canvas} />
+    return <canvas width="0" height="0" ref={this.canvas} />
   }
 
 }
