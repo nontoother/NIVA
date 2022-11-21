ad933fd3cb23c9d37154cb090416392fc4e04a1c
matiarasianipar <77903930+matiarasianipar@users.noreply.github.com>
2022-11-20 19:43:14 -0800

    CSS Fixes
    - FBX canvas should now fill whole screen
    - Moved FBX from App.js to Home.js

--- src/App.js
+++ src/App.js
@@ -1,134 +1,131 @@
-import React from 'react'
-import './App.css'
-// import Page from './component/index'
-// // import { useLoader } from '@react-three/fiber'
-// // import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-// // import { Canvas } from '@react-three/fiber'
-// // import { Environment, OrbitControls } from '@react-three/drei'
+// import React from 'react'
+// import './App.css'
+// // import Page from './component/index'
+// // // import { useLoader } from '@react-three/fiber'
+// // // import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+// // // import { Canvas } from '@react-three/fiber'
+// // // import { Environment, OrbitControls } from '@react-three/drei'
 
-// // const Scene = () => {
-// //   const fbx = useLoader(FBXLoader, 'NIVA.fbx')
-// //   return <primitive object={fbx} />
+// // // const Scene = () => {
+// // //   const fbx = useLoader(FBXLoader, 'NIVA.fbx')
+// // //   return <primitive object={fbx} />
+// // // }
+
+// // class App extends React.Component {
+// //   render () {
+// //     return(
+// //       <Page />
+// //     )
+// //   }
 // // }
 
-// class App extends React.Component {
-//   render () {
-//     return(
-//       <Page />
-//     )
-//   }
+// // export default App
+
+// // import ''./styles.css'
+// import { Canvas } from '@react-three/fiber'
+// import { useLoader } from '@react-three/fiber'
+// import { OrbitControls } from '@react-three/drei'
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+// import { Suspense } from 'react'
+// import { Environment } from '@react-three/drei'
+// // import axios from 'axios'
+// // import AudioAnalyser from '../voice/AudioAnalyser'
+// // import Notification from '../modal/Notification'
+// // import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+// // import './component/homepage/Home.css'
+// // import Notification from './component/modal/Notification'
+// // import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+// // import axios from 'axios'
+// // import AudioAnalyser from './component/voice/AudioAnalyser'
+
+// const Scene = () => {
+//   const fbx = useLoader(FBXLoader, 'NIVA.fbx')
+
+//   return <primitive object={fbx} scale={0.05} />
 // }
 
-// export default App
+// // const [audio, setAudio] = useState(null)
+// // const [access, setAccess] = useState(false)
+// // const [questionAudio, setQuestionAudio] = useState(null)
 
-// import ''./styles.css'
-import { Canvas } from '@react-three/fiber'
-import { useLoader } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import { Suspense } from 'react'
-import { Environment } from '@react-three/drei'
-// import axios from 'axios'
-// import AudioAnalyser from '../voice/AudioAnalyser'
-// import Notification from '../modal/Notification'
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
-// import './component/homepage/Home.css'
-// import Notification from './component/modal/Notification'
-// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
-// import axios from 'axios'
-// import AudioAnalyser from './component/voice/AudioAnalyser'
+// // const {
+// //   transcript,
+// //   resetTranscript
+// // } = useSpeechRecognition()
 
-const Scene = () => {
-  const fbx = useLoader(FBXLoader, 'NIVA.fbx')
+// // function sendQuestion (question) {
+// //   axios.post('http://127.0.0.1:5000/profile', null, {
+// //     params: {
+// //       questionText: question
+// //     }
+// //   })
+// //     .then((response) => {
+// //       const res = response.data
+// //       console.log(res)
+// //       setQuestionAudio(res.questionText)
+// //     }).catch((error) => {
+// //       if (error.response) {
+// //         console.log(error.response)
+// //         console.log(error.response.status)
+// //         console.log(error.response.headers)
+// //       }
+// //     })
+// // }
 
-  return <primitive object={fbx} scale={0.05} />
-}
+// // function checkPermissions () {
+// //   const permissions = navigator.mediaDevices.getUserMedia({audio: true, video: false})
+// //   permissions.then((stream) => {
+// //     setAccess(!access)
+// //   })
+// //     .catch((err) => {
+// //       setAccess(false)
+// //       console.log(`${err.name} : ${err.message}`)
+// //     })
+// // }
 
-// const [audio, setAudio] = useState(null)
-// const [access, setAccess] = useState(false)
-// const [questionAudio, setQuestionAudio] = useState(null)
+// // async function getMicrophone () {
+// //   const audio = await navigator.mediaDevices.getUserMedia({
+// //     audio: true,
+// //     video: false
+// //   })
+// //   SpeechRecognition.startListening({ continuous: false })
+// //   setAudio(audio)
+// // }
 
-// const {
-//   transcript,
-//   resetTranscript
-// } = useSpeechRecognition()
+// // function stopMicrophone () {
+// //   SpeechRecognition.stopListening()
+// //   if(transcript !== null) {
+// //     sendQuestion(transcript)
+// //   }
+// //   audio.getTracks().forEach(track => track.stop())
+// //   setAudio(null)
+// //   setQuestionAudio(null)
+// // }
 
-// function sendQuestion (question) {
-//   axios.post('http://127.0.0.1:5000/profile', null, {
-//     params: {
-//       questionText: question
-//     }
-//   })
-//     .then((response) => {
-//       const res = response.data
-//       console.log(res)
-//       setQuestionAudio(res.questionText)
-//     }).catch((error) => {
-//       if (error.response) {
-//         console.log(error.response)
-//         console.log(error.response.status)
-//         console.log(error.response.headers)
-//       }
-//     })
-// }
+// // function toggleMicrophone () {
+// //   resetTranscript()
+// //   if (audio) {
+// //     stopMicrophone()
+// //   } else {
+// //     getMicrophone()
+// //   }
+// // }
 
-// function checkPermissions () {
-//   const permissions = navigator.mediaDevices.getUserMedia({audio: true, video: false})
-//   permissions.then((stream) => {
-//     setAccess(!access)
-//   })
-//     .catch((err) => {
-//       setAccess(false)
-//       console.log(`${err.name} : ${err.message}`)
-//     })
-// }
+// // checkPermissions()
 
-// async function getMicrophone () {
-//   const audio = await navigator.mediaDevices.getUserMedia({
-//     audio: true,
-//     video: false
-//   })
-//   SpeechRecognition.startListening({ continuous: false })
-//   setAudio(audio)
-// }
-
-// function stopMicrophone () {
-//   SpeechRecognition.stopListening()
-//   if(transcript !== null) {
-//     sendQuestion(transcript)
-//   }
-//   audio.getTracks().forEach(track => track.stop())
-//   setAudio(null)
-//   setQuestionAudio(null)
-// }
-
-// function toggleMicrophone () {
-//   resetTranscript()
-//   if (audio) {
-//     stopMicrophone()
-//   } else {
-//     getMicrophone()
-//   }
-// }
-
-// checkPermissions()
-
-export default function App () {
-  return (
-    <div>
-      {/* <Notification isShowModal={access} askAccess={checkPermissions} />
-      {!audio}
-      {audio ? <AudioAnalyser audio={audio} /> : ''}
-      <p id='transcript'>{transcript}</p>
-      {questionAudio != null && <p id='transcript'>What I heard is: {questionAudio}</p>
-      } */}
-      <Canvas className="app-canvas">
-        <Suspense fallback={null}>
-          <Scene id='image' />
-          <OrbitControls />
-          <Environment background={true} preset={'apartment'} />
-        </Suspense>
-      </Canvas>
-    </div>
-  )
-};
+// export default function App () {
+//   return (
+//     <Canvas className="app-canvas">
+//       <Suspense fallback={null}>
+//         <Scene id='image' />
+//         <OrbitControls />
+//         <Environment
+//           files="https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@environment/public/img/venice_sunset_1k.hdr"
+//           background
+//           blur={0.5}
+//         />
+//         <directionalLight position={[3.3, 1.0, 4.4]} />
+//       </Suspense>
+//     </Canvas>
+//   )
+// };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Home from './component/homepage/Home'
+// import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-
-  <App />
-
+  <Home />
 )
