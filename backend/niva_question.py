dfe872e3f9ace48d6093ba7c1ff81a16285c66da
Rachel <675540150@qq.com>
2022-11-28 01:40:03 -0500

    update for path

--- backend/niva_question.py
+++ backend/niva_question.py
@@ -3,8 +3,8 @@ import json
 
 def get_answer_about_niva(question, question_answerer):
     context = ""
-    # with open('../context/niva_context.json', 'r') as niva_context_json:
-    with open('context/niva_context.json', 'r') as niva_context_json:
+    with open('../context/niva_context.json', 'r') as niva_context_json:
+    # with open('context/niva_context.json', 'r') as niva_context_json:
         context_list = json.load(niva_context_json)["niva_context"]
         for con in context_list:
             context += con
